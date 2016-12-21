305875ffc963586bbf74c63bcdb219d48093c751
Oskar Råhén <Oskar Rhn>
2016-12-21 11:58:39 +0100

    init api for the todo-app

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var router = express.Router();
+var todos = require("./../Todo")
+var todoItems = require("./../TodoItems");
 
-router.post("/gettodos", function(req,res){
-  res.send("HELLO");
-})
+
+router.get("/gettodos", function(req,res){
+  var response = {
+    id: 5,
+    name: "oskar"
+  }
+  res.json(response);
+});
+
+router.post("/gettodos", function(req, res){
+  var user = req.body.user;
+
+});
+
+
 
 module.exports = router;
