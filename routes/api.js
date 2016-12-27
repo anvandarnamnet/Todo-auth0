42a78596c8ba096d942b72454f5dd17c7b0ff699
Oskar Råhén <Oskar Rhn>
2016-12-27 19:20:33 +0100

    added delete todo item functionality

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,7 @@ router.post("/gettodos", function(req,res){
 
 router.post("/gettodoitems", function(req,res){
   var listId = req.body[0];
+
   todoItems.getTodosByListId(listId).then(function(cb){
     res.send(cb);
   })
@@ -22,9 +23,19 @@ router.post("/gettodoitems", function(req,res){
 
 router.post("/deletetodolist",function(req,res){
   var id = req.body[0];
+
   todos.removeTodoListByTodoId(id);
+
   var response =[];
   res.send(response);
+});
+
+router.post("/deletetodoitem", function(req,res){
+  var id = req.body[0];
+  todoItems.removeTodoById(id);
+  
+  var response = [];
+  res.send(response)
 });
 
 router.post("/addtodoitem", function(req,res){
@@ -42,14 +53,12 @@ router.post("/addtodolist", function(req,res){
   var date = req.body[1];
   var desc = req.body[0];
   var user = req.body[2];
+
   todos.addTodoListByUserId(user, desc, date).then(function(cb){
     var response = [];
     res.send(response);
   });
-
 });
-
-
 
 
 module.exports = router;
